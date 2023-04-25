0488b1d8684215a55c473a21a1127edd613337fb
farha123 <farhahussain55@gmail.com>
2023-04-24 20:46:58 -0400

    committing #2

--- src/test/java/Day14_04182023/T1_Yahoo_Screenshot.java
+++ src/test/java/Day14_04182023/T1_Yahoo_Screenshot.java
@@ -42,4 +42,5 @@ public class T1_Yahoo_Screenshot extends TestParent {
             logger.log(LogStatus.FAIL, "The checkbox is checked: " + isStaySignedInChecked);
         }
     }//end of test case 2
-}
+    //making a change
+}//end of class
